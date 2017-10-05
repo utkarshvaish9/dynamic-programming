fb83f9003a0642072fffa59dcc022a45a8baef12
Utkarsh Vaish <utkarshvaish9@gmail.com>
2017-10-05 08:28:02 +0530

    Update longest_subsequence.cpp

--- longest_subsequence.cpp
+++ longest_subsequence.cpp
@@ -2,17 +2,38 @@
 
 using namespace std;
 
-int n;
+#define clr(x) memset((x), 0, sizeof(x))
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define mp make_pair
+#define inputInt(n) int n;cin>>n
+#define fori(i, n) for(int i = 0; i < (int)(n); ++i)
+#define ford(i, n) for(int i = (int)(n) - 1; i >= 0; --i)
+#define for1(i, n) for(int i = 1; i <(int)(n); ++i)
+#define LL 				long long
+#define ULL 				unsigned long long
+#define pii 				pair<int,int>
+#define fi 				first
+#define se 				second
+#define vi 				vector<int>
+#define psb 				push_back
+#define ppb 				pop_back
+#define sz(x)				(int)(x).size()
+#define gc				getchar_unlocked
+#define present(container, element) (container.find(element) != container.end()) 
+#define cpresent(container, element) (find(all(container),element) != container.end()) //for vector
+
+
 int main()
 {
-	cin>>n;
+	inputInt(n);
 	int arr[n+1],ans[n+1];
 	fill_n(ans,n,1);
-	for(int i=0;i<n;i++)
+	fori(i,n)
 		cin>>arr[i];
-	for(int i=1;i<n;i++)
+	for1(i,n)
 	{
-		for(int j=0;j<i;j++)
+		fori(j,i)
 		{
 			if(arr[j]<arr[i])
 				ans[i]=max(ans[i],1+ans[j]);
